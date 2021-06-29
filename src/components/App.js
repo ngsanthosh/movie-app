c707f78a7067b1cfdc2c3264de3284c29a64450b
ngsanthosh <ngsanthosh657@gmail.com>
2021-06-29 20:30:19 +0530

    Added Home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 // import './App.css';
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       {/* <h1>Hello World!!</h1> */}
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Home = () => 
+    <div className="row">
+        <div className="col-md-4 col-xs-12"></div>
+        <div className="col-md-4 col-xs-12 text-center">
+            <img src="https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3" alt="Sample img" />
+            <h3>Made with ❤ by Santhosh</h3>
+            <p>Powered by React.</p>
+        </div>
+        <div className="col-md-4 col-xs-12"></div>
+    </div>
+
+
+export default Home;
