e8dfdd0ed9b7787a75e0ef2397d3ac4e7b3bd494
ngsanthosh <ngsanthosh657@gmail.com>
2021-07-02 19:46:30 +0530

    Added Route using react router dom

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,24 @@ import Header from "./Header";
 import Home from "./Home";
 import Moviecard from "./Moviecard"
 import Movielist from "./Movielist"
+import {BrowserRouter, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      {/* <h1>Hello World!!</h1> */}
-      <Header />
-      {/* <Home /> */}
-      {/* <Moviecard /> */}
-      <div className="container">
-        <Movielist />
+    <BrowserRouter>
+      <div className="App">
+        {/* <h1>Hello World!!</h1> */}
+        <Header />
+        {/* <Home /> */}
+        {/* <Moviecard /> */}
+        
+        <div className="container">
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/movies" component={Movielist} />
+         
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
