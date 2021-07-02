3f17619a0e23762f00c2f9f1be704bfd7d90db06
ngsanthosh <ngsanthosh657@gmail.com>
2021-07-02 21:14:47 +0530

    Added queryString for parsing URL

--- src/components/Movielist.jsx
+++ src/components/Movielist.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import Moviecard from './Moviecard';
 import Axios from 'axios';
+import queryString from 'query-string'
 
 class Movielist extends Component {
     state = { movies: [] }
 
     componentDidMount() {
         this.fetchmovies();
+        console.log(this.props);
     }
 
     fetchmovies=()=>{
-        let search="HOME ALONE"
-        Axios.get(`http://www.omdbapi.com/?s=${search}&apikey=7e383409`)
+        // let search="HOME ALONE"
+        let queryy =queryString.parse(this.props.location.search)
+        Axios.get(`http://www.omdbapi.com/?s=${queryy.s}&apikey=7e383409`)
         .then(came=>came.data)
         .then(datam=>datam.Search)
         .then(arraycame=>this.setState({movies:arraycame}))
