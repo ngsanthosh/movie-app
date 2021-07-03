042a1083df575661625757debfdd996ec8c6c84d
ngsanthosh <ngsanthosh657@gmail.com>
2021-07-03 11:37:18 +0530

    Styled Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,10 +11,11 @@ class Header extends Component {
                         <div className="col-md-4">
                             <h2><b>Movie Browser</b></h2> 
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-md-7">
                             <input type="search" placeholder="Type something here to search.." className="form-control"/>
                         </div>
-                        <div className="col-md-2">
+                        <div className="col-md-1">
+                            {/* <button type="submit">Search</button> */}
                             <h4>Favourites</h4> 
                         </div>
                     </div>
